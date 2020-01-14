f2959f60ed33abc2f241e7e1d493494221ba1709
BAHOHCEHC <bahohcehc@gmail.com>
2020-01-14 21:35:39 +0200

    feat:start project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { MDBBootstrapModule } from "angular-bootstrap-md";
+
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot(), AngularFontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
